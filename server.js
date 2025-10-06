69c236ddb00f006387bc3134f867ba1849c177e7
sfatbro786 <solaimanislamsifat@gmail.com>
2025-10-07 00:37:35 +0600

    changes for vercel

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
 //! Connect to MongoDB:
 connectDB();
@@ -45,8 +45,8 @@ app.use("/api/admin/users", userAdminRoutes);
 app.use("/api/admin/products", productAdminRoutes);
 app.use("/api/admin/orders", orderAdminRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
 module.exports = app;
